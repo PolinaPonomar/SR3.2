432f1f093013c77be98b6c409cf98c7477093ef0
tolyone <tolybook@gmaill.com>
2018-12-21 11:16:54 +0300

    1-4

--- main.cpp
+++ main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+class CLASS{
+private:
+    int x;
+    CLASS*y;
+public:
+
+    CLASS(int a) : x(a) {
+        y = nullptr;
+        std::cout << "Конструктор\n";
+        std::cout << " X = " << x << "; адрес класса = " << y <<std::endl;
+
+    }
+
+    void push (int a) {
+        CLASS z(a);
+        y = &z;
+        std::cout << "PUSH:\n";
+        std::cout << " X = " << x << "; адрес класса = " << y <<std::endl;
+
+
+
+    }
+
+    ~CLASS() {
+        
+        std::cout << "Деструктор\n";
+    }
+
+
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int*a = new int;
+    *a = 6;
+    CLASS n(*a);
+    int*b = new int;
+    *b = 7;
+    n.push(*b);
+
     return 0;
 }
